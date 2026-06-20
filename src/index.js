19e596fff1b54c6256cb27fa4ac6d6c1c3a3c0da
iFamished <me@ifamished.com>
2026-06-20 23:47:34 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Import global + component styles
+import "./styles/globals.css"
+import "./styles/components.css"
+
+// Export components
+export { default as GlassCard } from "./components/GlassCard"
+export { default as GlassButton } from "./components/GlassButton"
+export { default as Navbar } from "./components/Navbar"
+export { default as Footer } from "./components/Footer"
