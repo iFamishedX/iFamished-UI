d0e684c560aecf7fb0da4ad6c75d1b08c86cb90d
iFamished <me@ifamished.com>
2026-06-22 18:01:17 +0000

    Added usePageTitle hook

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export { default as GlassCard } from "./components/GlassCard"
 export { default as GlassButton } from "./components/GlassButton"
 export { default as Navbar } from "./components/Navbar"
 export { default as Footer } from "./components/Footer"
+
+// Export hooks
+export * from "./hooks/usePageTitle"
