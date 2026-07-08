033bb126446bd75bf21cf7de9c744b0ccc76be03
iFamished <me@ifamished.com>
2026-07-08 18:47:05 +0000

    Add component

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function InputBox({ value, onChange, placeholder }) {
+  return (
+    <div className="inputbox">
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export { default as Footer } from "./components/Footer"
 export { default as GlassButton } from "./components/GlassButton"
 export { default as GlassCard } from "./components/GlassCard"
 export { default as Icon } from "./components/Icon"
+export { default as InputBox } from "./components/InputBox";
 export { default as MarkdownRenderer } from "./components/MarkdownRenderer"
 export { default as Navbar } from "./components/Navbar"
 export { default as ScrollToTop } from "./components/ScrollToTop"
